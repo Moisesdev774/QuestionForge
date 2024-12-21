ee4c7e3863fa1462a787b99ecac376f8cf4a6c4d
Moisés Bonilla <moisesbonilla2022@outlook.com>
2024-12-20 20:19:38 -0600

    Se creó el proyecto QuestionForge.WebAPI, se creó y programó el controlador UsuarioController, se medoficó el launchSetting.json y se configuró el Program para la inyección de dependencias

---
+++ QuestionForge.WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuestionForge.EntidadesDeNegocio;
+using QuestionForge.LogicaDeNegocio;
+
+namespace QuestionForge.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UsuarioBL _usuarioBL;
+
+        public UsuarioController(UsuarioBL usuarioBL)
+        {
+            _usuarioBL = usuarioBL;
+        }
+
+        [HttpGet("ObtenerTodos")]
+        public async Task<IActionResult> ObtenerTodosLosUsuarios()
+        {
+            try
+            {
+                var usuarios = await _usuarioBL.ObtenerTodosLosUsuariosAsync();
+                return Ok(usuarios);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("Registrar")]
+        public IActionResult RegistrarUsuario([FromBody] Usuario usuario)
+        {
+            try
+            {
+                _usuarioBL.RegistrarUsuarioAsync(usuario.Nombre, usuario.Password);
+                return Ok("Usuario registrado con éxito.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> VerificarCredenciales([FromBody] Usuario usuario)
+        {
+            try
+            {
+                var resultado = await _usuarioBL.VerificarCredencialesAsync(usuario.Nombre, usuario.Password);
+                return Ok(resultado);
+            }
+            catch (Exception ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+    }
+}
