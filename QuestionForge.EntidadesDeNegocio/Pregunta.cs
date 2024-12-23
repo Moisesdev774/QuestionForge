2cfd660572d4d0ad2ef7246a3f09482d685a0971
Moisés Bonilla <moisesbonilla2022@outlook.com>
2024-12-23 11:16:55 -0600

    Ajustes a EntidadesDeNegocio

---
+++ QuestionForge.EntidadesDeNegocio/Pregunta.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionForge.EntidadesDeNegocio
+{
+    public class Pregunta
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        [Required]
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public bool Cerrada { get; set; }
+        public List<Respuesta> Respuestas { get; set; } = new List<Respuesta>();
+
+    }
+}
---
+++ QuestionForge.EntidadesDeNegocio/Respuesta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionForge.EntidadesDeNegocio
+{
+    public class Respuesta
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdPregunta { get; set; }
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contenido { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
